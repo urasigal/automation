01edfdb19017d83d3f2a9066207ded4e2bdf8b21
urasigal <urasigal@gmail.com>
2015-12-09 14:38:30 +0200

    add HLS testing and add it to POM file

--- src/test/java/com/zixi/drivers/FFMPEGImageStatisticTestDriver.java
+++ src/test/java/com/zixi/drivers/FFMPEGImageStatisticTestDriver.java
@@ -17,7 +17,9 @@ public class FFMPEGImageStatisticTestDriver extends BroadcasterLoggableApiWorker
 	private static final String hostName   = "10.7.0.150";
 	private static final int    portNumber = 4445;
 	private static final int    attempts   = 20;
+	private static final int    hlsAttempts   = 200;
 	private static final String fromUser = "get";
+	private static final String HLS = "hls";
 	private Socket clientSocket;
 	private PrintWriter out;
 	private BufferedReader in;
@@ -56,6 +58,71 @@ public class FFMPEGImageStatisticTestDriver extends BroadcasterLoggableApiWorker
 						fromServer = in.readLine();
 						System.out.println("Expected is accepted " + fromServer);
 						
+					}
+				}
+			}
+			
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host " + hostName);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to "
+					+ hostName);
+		}
+		finally{
+			try {
+				Reporter.log("FFMPEG success measurement relation: " + sum+ " / " + loopCnt);
+			if (clientSocket != null)
+				clientSocket.close();
+			if (out != null)
+				out.close();
+			if (in!=null)
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if ((sum / attempts) >= 0.9)
+		{
+			return "good";
+		}
+		else 
+			return "bad";
+	}
+	
+	public String testStatistic(String mode) 
+	{
+		// TODO Auto-generated method stub
+		long sum = 0;
+		loopCnt = 0;
+		try {
+
+			clientSocket = new Socket(hostName, portNumber);
+			clientSocket.setSoTimeout(120000);
+			 
+             out = new PrintWriter(clientSocket.getOutputStream(), true);
+		 
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+			String fromServer = in.readLine();
+			System.out.println("Expected is connected " + fromServer);
+			if (fromServer.equals("connected"))
+			{
+				for (loopCnt = 0; loopCnt < hlsAttempts; loopCnt ++ )
+				{
+					out.println(mode);
+					fromServer = in.readLine();
+					System.out.println("Expected is output " + fromServer);
+					if(fromServer.equals("output"))
+					{
+						fromServer = in.readLine();
+						System.out.println("Expected is number, actual is: " + fromServer);
+						if(fromServer.equals("1"))
+						{
+							sum += Long.parseLong(fromServer, 10);   
+						}
+						fromServer = in.readLine();
+						System.out.println("Expected is accepted, actual is: " + fromServer);
 					}
 				}
 			}
---
+++ src/test/java/com/zixi/testing/FFMPEGImageStatisticHlsTest.java
@@ -0,0 +1,30 @@
+package com.zixi.testing;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.zixi.drivers.FFMPEGImageStatisticTestDriver;
+
+public class FFMPEGImageStatisticHlsTest extends BaseTest{
+
+	
+	@BeforeClass
+	public void testInit() {
+
+		// This is a test driver.
+		testDriver = new FFMPEGImageStatisticTestDriver();
+	}
+
+	// The goal of the test is to measure quality of a Zixi delivered video by using FFMPEG.
+	// The quality is estimated by a number of a stream probing (FFMPEG) and then getting a ratio between a successful probing to failed attempts.
+	@Parameters({"mode", "testid"})
+	@Test
+	public void broadcasterSingleInputStreamStatisticAnilyzer(String mode, String testid) throws InterruptedException {
+		this.testid = testid;
+		Assert.assertEquals(
+				((FFMPEGImageStatisticTestDriver) testDriver)
+						.testStatistic(mode), "good");
+	}
+}
