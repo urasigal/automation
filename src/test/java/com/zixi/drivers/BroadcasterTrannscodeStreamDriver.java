b77ca1fe79f8ea89ec84fc44599d53e85b9e83f2
urasigal <urasigal@gmail.com>
2016-02-22 15:54:31 +0200

    add coments

--- src/test/java/com/zixi/drivers/BroadcasterTrannscodeStreamDriver.java
+++ src/test/java/com/zixi/drivers/BroadcasterTrannscodeStreamDriver.java
@@ -51,7 +51,7 @@ public class BroadcasterTrannscodeStreamDriver extends
 		    }
 		  }
 		
-		//Zhopa
+		// Transcode a stream zhopa
 		return apiworker.sendGet("http://" + login_ip + ":" + uiport
 				+ "/zixi/add_stream.json?" + rtype + type + "&" + rid + id
 				+ "&" + rmatrix + matrix + "&" + rmax_outputs + max_outputs
