046b0a64bea61cf0429491504a5066b285c171c4
urasigal <urasigal@gmail.com>
2016-03-27 12:25:49 +0300

    add video to centos7 trunk

---
+++ src/test/java/com/zixi/testing/VisualQualityTest.java
@@ -0,0 +1,30 @@
+package com.zixi.testing;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.zixi.drivers.*;
+
+public class VisualQualityTest extends BaseTest{
+	
+	
+	@BeforeClass
+	public void testInit() {
+		testDriver = new VisualQualityDriver();
+	}
+
+	@Parameters({"testid"})
+	@Test
+	public void broadcasterRtmpPullTest(String testid) throws InterruptedException {
+		this.testid = testid;
+		
+		
+		testParameters = buildTestParametersString(new String[] {"testid"}, 
+		new String[] {testid});
+		
+		Assert.assertEquals(((VisualQualityDriver) testDriver)
+				.testVideo(), "added");
+	}
+}
