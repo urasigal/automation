3b97c268afae780b3790678a8cd1e72b174fb58b
urasigal <urasigal@gmail.com>
2016-12-25 22:49:11 +0200

    add broadcaster push output redundant host  test

---
+++ src/test/java/com/zixi/testing/BroadcasterPushOutStreamCreationGeneralTest.java
@@ -0,0 +1,45 @@
+package com.zixi.testing;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.zixi.drivers.drivers.BroadcaserSingleOutputStreamDeletionDriver;
+import com.zixi.drivers.drivers.BroadcasterPushOutStreamCreationDriver;
+import com.zixi.drivers.drivers.TestDriver;
+
+public class BroadcasterPushOutStreamCreationGeneralTest extends BaseTest {
+	
+	private TestDriver testDriver;
+
+	@BeforeClass
+	public void testInit() {
+		testDriver = new BroadcasterPushOutStreamCreationDriver();
+	}
+
+	@Parameters({ "userName", "userPass", "login_ip", "host", "latency", "fec_force", "session", "fec_adaptive", "nic", "fec_block", "type",
+		"snames", "fec_aware", "fec_overhead", "stream", "port", "uiport", "alias", "id", "link1", "link2", "bond_links", "testid" })
+	
+	@Test
+	public void broadcasterPushOutCreationWithTwoRedundantHosts(String userName, String userPass, String login_ip, String host, String latency, String fec_force,
+	String session, String fec_adaptive, String nic, String fec_block, String type, String snames, String fec_aware, String fec_overhead,
+	String stream, String port, String uiport, String alias, String id, String link1, String link2, String bond_links, String testid) throws Exception {
+		
+		this.version = productAboutDriver.getBroadcasterVersion(login_ip, uiport, userName, userPass);
+		
+		sutProcessId = BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "pidof zixi_broadcaster");
+		
+		testParameters = buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "host", "latency", "fec_force", "session", "fec_adaptive", "nic", "fec_block", "type",
+				"snames", "fec_aware", "fec_overhead", "stream", "port", "uiport", "alias", "id", "link1", "link2", "bond_links", "testid"}, 
+		
+		new String[] { userName, userPass, login_ip, host, latency, fec_force, session, fec_adaptive, nic, fec_block, type, snames, fec_aware, fec_overhead, 
+		stream, port, uiport, alias, id, link1, link2, bond_links, testid });
+		
+		Assert.assertEquals(((BroadcasterPushOutStreamCreationDriver) testDriver).testIMPLRedund(userName, userPass, login_ip, host, latency, fec_force,
+		session, fec_adaptive, nic, fec_block, type, snames, fec_aware, fec_overhead, stream, port, uiport, alias, id, link1, link2, bond_links), "Output " + id + " added.");
+		
+		// Checking if broadcaster has crashes while execution of the test.
+		Assert.assertEquals(sutProcessId, BroadcaserSingleOutputStreamDeletionDriver.getPid("root", "zixiroot1234", login_ip, "22", "pidof zixi_broadcaster"));
+	}
+}
