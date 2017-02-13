d485a3e79f9ce06bafc49963e380f3932f15c5f2
tester <tester@192.168.0.14>
2017-02-13 07:20:38 -0800

    add transcoding stuff

--- src/test/java/com/zixi/testing/BroadcasterAddIntelTranscoderProfileTest.java
+++ src/test/java/com/zixi/testing/BroadcasterAddIntelTranscoderProfileTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import com.zixi.drivers.drivers.BroadcasterAddTranscoderProfileDriver;
 
 public class BroadcasterAddIntelTranscoderProfileTest extends BaseTest {
@@ -13,32 +14,32 @@ public class BroadcasterAddIntelTranscoderProfileTest extends BaseTest {
 		testDriver = new BroadcasterAddTranscoderProfileDriver();
 	}
 
-	@Parameters({"userName", "userPass", "login_ip", "uiport", "mode", "profile_name","enc", "bitrate", "gop", "fixed_gop", "closed_gop", "copy_gop",  "performance",
-	"b_frames", "frame_type", "profile", "level", "bitrate_mode", "ref_frames", "hrd", "idr_int", "cavlc", "brightness", "contrast", "fps",
-	"width", "height","crf", "tune", "max_bitrate", "testid" })
-	
+	@Parameters({"userName", "userPass", "login_ip", "uiport", "mode", "profile_name", "enc", "bitrate", "gop", "fixed_gop", "closed_gop", "copy_gop",  
+	"performance", "b_frames", "frame_type", "profile", "level", "bitrate_mode",
+	"ref_frames", "hrd", "idr_int", "cavlc", "brightness", "contrast", "fps", "width", "height","crf", "tune", "use_hw", "max_bitrate", "testid" })
 	@Test
 	public void broadcasterSingleInputStreamstatisticAnilyzer(String userName, String userPass, String login_ip, String uiport,String mode, String profile_name,
 	String enc, String bitrate, String gop, String fixed_gop, String closed_gop, String copy_gop, String performance,
 	String b_frames, String frame_type, String profile, String level, String bitrate_mode,
 	String ref_frames, String hrd, String idr_int, String cavlc, String brightness, String contrast, String fps,
-	String width, String height, String crf, String tune, String max_bitrate, String testid)
-	throws Exception {
+	String width, String height, String crf, String tune, String use_hw,  String max_bitrate, String testid) throws Exception {
+
 		
-	// Retrieve the product version. Parameters: 1 - host, 2 - user interface port, 3 - product login name, 4 - product login password.
+		// Retrieve the product version. Parameters: 1 - host, 2 - user interface port, 3 - product login name, 4 - product login password.
 		this.version = productAboutDriver.getBroadcasterVersion(login_ip, uiport, userName, userPass);
-		
+				
 		testParameters = buildTestParametersString(new String[] {"userName", "userPass", "login_ip", "uiport", "mode", "profile_name",
-		"enc", "bitrate", "gop", "fixed_gop", "closed_gop", "copy_gop",  "performance", "b_frames", "frame_type", "profile", "level", "bitrate_mode",
-		"ref_frames", "hrd", "idr_int", "cavlc", "brightness", "contrast", "fps",
-		"width", "height","crf", "tune", "max_bitrate", "testid"}, 
-		new String[] {userName, userPass, login_ip, uiport, mode, profile_name, enc, bitrate, gop, fixed_gop, closed_gop, copy_gop, performance,
+		"enc", "bitrate", "gop", "fixed_gop", "closed_gop", "copy_gop",  "performance",
+		"b_frames", "frame_type", "profile", "level", "bitrate_mode", "ref_frames", "hrd", "idr_int", "cavlc", "brightness", "contrast", "fps",
+		"width", "height","crf", "tune", "use_hw",  "max_bitrate", "testid"}, 
+						
+		new String[] {userName, userPass, login_ip, uiport, mode, profile_name,
+		enc, bitrate, gop, fixed_gop, closed_gop, copy_gop, performance,
 		b_frames, frame_type, profile, level, bitrate_mode, ref_frames, hrd, idr_int, cavlc, brightness, contrast, fps,
-		width, height,crf, tune, max_bitrate, testid});
+		width, height,crf, tune, use_hw,  max_bitrate, testid});
 		
 		Assert.assertEquals(((BroadcasterAddTranscoderProfileDriver) testDriver).testIMPL(userName, userPass, login_ip, uiport, mode, profile_name,
-		enc, bitrate, gop, fixed_gop, closed_gop, copy_gop, performance,b_frames, frame_type, profile, level, bitrate_mode,
-		ref_frames, hrd, idr_int, cavlc, brightness, contrast, fps,
-		width, height,crf, tune, max_bitrate), "Profile added, existing transcoded streams may be restarted.");
+		enc, bitrate, gop, fixed_gop, closed_gop, copy_gop, performance, b_frames, frame_type, profile, level, bitrate_mode,
+		ref_frames, hrd, idr_int, cavlc, brightness, contrast, fps, width, height,crf, tune, use_hw, max_bitrate), "Profile added, existing transcoded streams may be restarted.");
 	}
 }
