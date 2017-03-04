c76f38292c145a2cde785b302f10fe12d7c16fc0
tester <tester@192.168.0.11>
2017-03-04 08:34:27 -0800

    extend maxwell suite

--- src/test/java/com/zixi/maxwell/testing/BroadcasterAdaptiveStairsImpairmentTest.java
+++ src/test/java/com/zixi/maxwell/testing/BroadcasterAdaptiveStairsImpairmentTest.java
@@ -15,10 +15,12 @@ public class BroadcasterAdaptiveStairsImpairmentTest extends BaseTest {
 	public void testInit() {  testDriver = new BroadcasterAdaptiveStairsImpairmentDriver(); }
 
 	// Test parameters.
-	@Parameters({"maxwell_address", "standart_impairment_server_api_port", "flow_match_control_setmatch", "impairment_control_setimpair", "testid"})
+	@Parameters({"maxwell_address", "standart_impairment_server_api_port", "flow_match_control_setmatch", "impairment_control_setimpair", 
+		         "userName", "userPassword", "login_ip", "uiport", "id", "testid"})
 	@Test
-	public void broadcasterSingleStreamRemoving(String maxwell_address, String standart_impairment_server_api_port, String flow_match_control_setmatch, 
-	String impairment_control_setimpair, String testid) throws Exception {
+	public void broadcasterSingleStreamRemoving(String maxwell_address, String standart_impairment_server_api_port, String flow_match_control_setmatch,
+			String impairment_control_setimpair, String userName, String userPassword, String login_ip, String uiport, String id, String testid) throws Exception {
+		
 		//Print this class name to the log file.
 		getLoggerInstance().info(getClass().getName());
 		
@@ -29,8 +31,8 @@ public class BroadcasterAdaptiveStairsImpairmentTest extends BaseTest {
 		"impairment_control_setimpair", "testid" }, 
 		new String[] { maxwell_address, standart_impairment_server_api_port, flow_match_control_setmatch, impairment_control_setimpair, testid });
 		
-		driverReslut = ((BroadcasterAdaptiveStairsImpairmentDriver) testDriver).testIMPL(maxwell_address, standart_impairment_server_api_port, 
-		flow_match_control_setmatch, impairment_control_setimpair); 
+		driverReslut = ((BroadcasterAdaptiveStairsImpairmentDriver) testDriver).testIMPL( maxwell_address,  standart_impairment_server_api_port, 
+		flow_match_control_setmatch, impairment_control_setimpair,  userName,  userPassword,  login_ip,  uiport,  id); 
 		
 		Assert.assertEquals(driverReslut.getResult(), "GOOD");
 		// Checking if broadcaster has crashes while execution of the test.
