cbe07e721d74586f45c38bff33018e1e4fb8f34c
tester <tester@192.168.0.10>
2017-05-23 09:19:07 -0700

    Change BroadcasterInputStreamManipulatorTest: make it record a stream for specific duration then stop recording.

--- src/test/java/com/zixi/testing/BroadcasterInputStreamManipulatorTest.java
+++ src/test/java/com/zixi/testing/BroadcasterInputStreamManipulatorTest.java
@@ -33,19 +33,19 @@ public class BroadcasterInputStreamManipulatorTest extends BaseTestZixiMainCompo
 	}
 	
 	
-	@Parameters({ "userName", "userPass","login_ip", "uiport", "id", "on", "testid" })
+	@Parameters({ "userName", "userPass","login_ip", "uiport", "id", "duration_ms", "testid" })
 	@Test
-	public void broadcasterStartStopRecording(String userName, String userPass, String login_ip, String uiport, String id, String on, String testid)
+	public void broadcasterStartStopRecording(String userName, String userPass, String login_ip, String uiport, String id, String duration_ms, String testid)
 	throws Exception {
 		
 		 productAboutDriver.getBroadcasterVersion(login_ip, uiport, userName, userPass);
 		
 		sutProcessId 	= BroadcaserSingleOutputStreamDeletionDriver.getPid("root", "zixiroot1234", login_ip, "22", "pidof zixi_broadcaster");
 		
-		buildTestParametersString(new String[] { "userName", "userPass","login_ip", "uiport", "id", "on", "testid" }, 
-		new String[] { userName, userPass, login_ip, uiport, id, on, testid });
+		buildTestParametersString(new String[] { "userName", "userPass","login_ip", "uiport", "id", "duration_ms", "testid" }, 
+		new String[] { userName, userPass, login_ip, uiport, id, duration_ms, testid });
 		
-		driverReslut 	= ((BroadcasterInputStreamManipulatorDriver) testDriver).testIMPL( userName, userPass, login_ip, uiport, id, on );
+		driverReslut 	= ((BroadcasterInputStreamManipulatorDriver) testDriver).testIMPL( userName, userPass, login_ip, uiport, id, duration_ms );
 		
 		Assert.assertEquals(driverReslut.getResult(), "1");
 		
