404fd612716ee919258dc21bf8cbeaad64367aa5
tester <tester@192.168.0.14>
2017-06-22 09:59:50 -0700

    add optional parameter to feeder output deletion test.

--- src/test/java/com/zixi/testing/FeederOutputDeletioinTest.java
+++ src/test/java/com/zixi/testing/FeederOutputDeletioinTest.java
@@ -2,6 +2,7 @@ package com.zixi.testing;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import com.zixi.drivers.drivers.FeederOutputDeletionDriver;
@@ -11,16 +12,17 @@ public class FeederOutputDeletioinTest extends BaseTestZixiMainComponents{
 	@BeforeClass
 	public void testInit() {testDriver = new FeederOutputDeletionDriver();}
 
-	@Parameters({ "userName", "userPass", "login_ip", "uiport", "id", "mip", "port", "ip", "prog", "chan", "type","host" ,"testid"})
+	@Parameters({ "userName", "userPass", "login_ip", "uiport", "id", "mip", "port", "ip", "prog", "chan", "type","host", "push_port" ,"testid"})
 	@Test
 	public void broadcasterSingleStreamRemoving(String userName, String userPass, String login_ip, String uiport, String id,
-	String mip, String port, String ip, String prog, String chan, String type, String host,String testid) throws Exception {
+	String mip, String port, String ip, String prog, String chan, String type, String host, @Optional("2088") String push_port, String testid) throws Exception {
 		
 		// Writes test results to the TestLink.
-		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport", "id", "mip","port", "ip", "prog", "chan", "type","host" ,"testid" }, 
-		new String[] { userName, userPass, login_ip, uiport, id, mip, port, ip, prog, chan, type,host ,testid });
+		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport", "id", "mip","port", "ip", "prog", "chan", "type","host", "push_port" ,"testid" }, 
+		new String[] { userName, userPass, login_ip, uiport, id, mip, port, ip, prog, chan, type,host, push_port, testid });
 		
-		driverReslut = ((FeederOutputDeletionDriver) testDriver).testIMPL(userName, userPass, login_ip, uiport, id, mip, port, ip, prog, chan, type, host);
+		driverReslut = ((FeederOutputDeletionDriver) testDriver).testIMPL(userName, userPass, login_ip, uiport, id, mip, port, 
+		ip, prog, chan, type, host, push_port);
 		
 		Assert.assertEquals(driverReslut.getResult(), "Output deleted.");
 	}
