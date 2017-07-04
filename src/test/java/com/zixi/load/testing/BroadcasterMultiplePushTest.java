ea5c07f481ea91734b62bfa5ecc1cc7242e520da
tester <tester@192.168.0.12>
2017-07-03 18:19:30 -0700

    fix mixed_dtls_non_dtls.xml

--- src/test/java/com/zixi/load/testing/BroadcasterMultiplePushTest.java
+++ src/test/java/com/zixi/load/testing/BroadcasterMultiplePushTest.java
@@ -48,7 +48,7 @@ public class BroadcasterMultiplePushTest extends BaseTestZixiMainComponents{
 		"force_p2p", "mcast_ip", "mcast_force", "mcast_port", "type_bx1", "uiport_bx1", "analyze", "mcast_ttl", "id_bx1", "mcast_out",
 		"complete", "max_outputs", "on", "password", "host", "latency_bx2", "fec_force", "session", "fec_adaptive",
 		"nic", "fec_block", "type_bx2", "snames", "fec_aware", "fec_overhead", "stream", "port", "uiport_bx2", "alias", "id_bx2" ,
-		"number_of_streams", "mixed", "testid"}, 
+		"number_of_streams", "", "testid"}, 
 		
 		new String[] { userName_bx1,  userName_bx2,  userPass_bx1,  userPass_bx2,  login_ip_bx1, 
 		 login_ip_bx2,  latency_bx1,  time_shift , force_p2p,  mcast_ip,  mcast_force,  mcast_port, 
