e4623a1c377ab3f316f25db2d3ae65bfbdc977c8
urasi <urasi@192.168.0.10>
2017-07-31 11:21:46 -0500

    Add new FFMpeg test with Maxwell log analyzer  to g_10_50_push_87.xml

---
+++ src/test/java/com/zixi/testing/FFMPEGImageStatisticWithMaxwellLogTest.java
@@ -0,0 +1,35 @@
+package com.zixi.testing;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.zixi.drivers.drivers.FFMPEGImageStatisticTestDriver;
+import com.zixi.maxwell.drivers.BroadcasterPushG1050ConfigurationDriver;
+
+public class FFMPEGImageStatisticWithMaxwellLogTest extends BaseTestZixiMainComponents{
+	
+	@BeforeClass
+	public void testInit() {
+		// This is a test driver.
+		testDriver = new FFMPEGImageStatisticTestDriver();
+	}
+
+	// The goal of the test is to measure quality of a Zixi delivered video by using FFMPEG.
+	// The quality is estimated by a number of a stream probes (FFMPEG) and then getting a ratio between a successful probing to failed attempts.
+	@Parameters({"testid"})
+	@Test
+	public void broadcasterSingleInputStreamStatisticAnilyzer(String testid) throws InterruptedException {
+		
+		// Transfer a log file from Maxwell to 10.7.0.150
+		BroadcasterPushG1050ConfigurationDriver.transferLogFile();
+		
+		buildTestParametersString(new String[] { "testid" }, 
+		new String[] {"testid" });
+		
+		driverReslut = ((FFMPEGImageStatisticTestDriver) testDriver).testStatistic().getResultObj();
+		Assert.assertEquals(driverReslut.getResult(), "good");
+		driverReslut.touchResutlDescription(((FFMPEGImageStatisticTestDriver) testDriver).testStatistic(1).getResultObj().getResult());		
+	}
+}
