859824bf2d36aa2d12bd0b6ddee9075b58995e54
urasigal@gmail.com <urasigal@gmail.com>
2017-12-10 10:48:32 -0400

    add receiverFallback.xml

---
+++ src/test/java/com/zixi/testing/ReceiverAddSourceToOutputTest.java
@@ -0,0 +1,29 @@
+package com.zixi.testing;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.zixi.drivers.drivers.ReceiverAddSourceToOutputDriver;
+
+
+public class ReceiverAddSourceToOutputTest  extends BaseTestZixiMainComponents{
+
+	@BeforeClass
+	public void testInit() { testDriver = new ReceiverAddSourceToOutputDriver();}
+
+	@Parameters({ "userName", "userPass", "login_ip", "uiport",  "inputStreamIdArg", "outputStreamIdArg", "fallback", "testid"})
+	@Test
+	public void feederOutputToBxTest(String userName, String userPass, String login_ip, String uiport, String inputStreamIdArg, String outputStreamIdArg, String fallback, String testid)
+	throws Exception {
+		
+		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport",  "inputStreamIdArg", "outputStreamIdArg", "fallback", "testid" }, 
+				
+		new String[] { userName, userPass, login_ip, uiport,  inputStreamIdArg,  outputStreamIdArg, fallback, testid });
+			
+		// String streamForDeletion = ((ReceiverPullInputStreamDeletionDriver) testDriver).testIMPL(userName, userPassword, login_ip, uiport, id);
+		
+		driverReslut = ((ReceiverAddSourceToOutputDriver) testDriver).testIMPL(userName, userPass, login_ip, uiport,  inputStreamIdArg,  outputStreamIdArg, fallback);
+		Assert.assertEquals(driverReslut.getResult(), "Input assigned to output stream.");
+	}
+}
