6ffb45f94f1a68c67bb896fb34e123e24982f248
urasigal@gmail.com <urasigal@gmail.com>
2017-12-17 13:22:26 -0400

    add "remoteBX_UI_port" argument to "ReceiverDeleteActiveOutputSourceTest" and corresponding test suite file.

--- src/test/java/com/zixi/testing/ReceiverDeleteActiveOutputSourceTest.java
+++ src/test/java/com/zixi/testing/ReceiverDeleteActiveOutputSourceTest.java
@@ -13,13 +13,13 @@ public class ReceiverDeleteActiveOutputSourceTest  extends BaseTestZixiMainCompo
 	@BeforeClass
 	public void testInit() { testDriver = new ReceiverDeleteActiveOutputSourceDriver();}
 
-	@Parameters({ "userName", "userPass", "login_ip", "uiport", "outputStreamIdArg", "testid"})
+	@Parameters({ "userName", "userPass", "login_ip", "uiport", "outputStreamIdArg", "remoteBX_UI_port", "testid"})
 	@Test
-	public void addSourceToReceiverOutput(String userName, String userPass, String login_ip, String uiport, String outputStreamIdArg, String testid)
+	public void addSourceToReceiverOutput(String userName, String userPass, String login_ip, String uiport, String outputStreamIdArg, String remoteBX_UI_port, String testid)
 	throws Exception {
-		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport", "outputStreamIdArg", "testid" }, 
-		new String[] { userName, userPass, login_ip, uiport, outputStreamIdArg, testid });												  
-		driverReslut = ((ReceiverDeleteActiveOutputSourceDriver) testDriver).testIMPL(userName, userPass, login_ip, uiport, outputStreamIdArg);
+		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport", "outputStreamIdArg", "remoteBX_UI_port", "testid" }, 
+		new String[] { userName, userPass, login_ip, uiport, outputStreamIdArg, remoteBX_UI_port, testid });												  
+		driverReslut = ((ReceiverDeleteActiveOutputSourceDriver) testDriver).testIMPL(userName, userPass, login_ip, uiport, outputStreamIdArg, remoteBX_UI_port);
 		System.out.println(driverReslut.getResult());
 	}
 }
