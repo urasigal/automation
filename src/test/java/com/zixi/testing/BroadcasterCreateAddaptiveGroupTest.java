401c3734b9b7b9790ea40afa6d195fd764ef8b40
urasigal@gmail.com <urasigal@gmail.com>
2018-04-24 15:08:22 -0300

    add change_folder_chunks

--- src/test/java/com/zixi/testing/BroadcasterCreateAddaptiveGroupTest.java
+++ src/test/java/com/zixi/testing/BroadcasterCreateAddaptiveGroupTest.java
@@ -15,12 +15,12 @@ public class BroadcasterCreateAddaptiveGroupTest extends BaseTestZixiMainCompone
 	public void testInit() { testDriver = new BroadcasterCreateAddaptiveGroupDriver(); }
 	
 	@Parameters({ "userName", "userPass", "login_ip", "uiport", "name", "record", "zixi", "hls", "hds", "mpd", "mmt", "compress_zixi",
-	"multicast", "streams", "bitrates", "max_time", "remux", "iframes", "file", "scte35", "ts_split", "order_ascending",  "testid" })
+	"multicast", "streams", "bitrates", "max_time", "remux", "iframes", "file", "scte35", "ts_split", "order_ascending", "change_folder_chunks", "testid" })
 	@Test
 	public void receiverOutputUdpTest(String userName, String userPass, String login_ip, String uiport, String name, String record,
 	String zixi, String hls, String hds, String mpd, String mmt, String compress_zixi, String multicast, String streams,
 	String bitrates, String max_time, String remux, String iframes, String file, String scte35, String ts_split, 
-	String order_ascending, String testid)throws Exception {
+	String order_ascending, String change_folder_chunks, String testid)throws Exception {
 		
 		sutProcessId = BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "pidof zixi_broadcaster");
 		// Retrieve the product version. Parameters: 1 - host, 2 - user interface port, 3 - product login name, 4 - product login password.
@@ -28,12 +28,12 @@ public class BroadcasterCreateAddaptiveGroupTest extends BaseTestZixiMainCompone
 		memOnStart = BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "ps v `pidof zixi_broadcaster` | tail -n 1 |  awk '{print $8}'");
 		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport", "name",
 		"record", "zixi", "hls", "hds", "mpd", "mmt", "compress_zixi", "multicast", "streams", "bitrates", "max_time", "remux", "iframes", "file",
-		"scte35", "ts_split", "order_ascending", "testid" }, 
+		"scte35", "ts_split", "order_ascending", "change_folder_chunks", "testid" }, 
 		new String[] { userName, userPass, login_ip, uiport, name,record, zixi, hls, hds, mpd, mmt, compress_zixi,
-		multicast, streams, bitrates, max_time, remux, iframes, file, scte35, ts_split, order_ascending, testid });
+		multicast, streams, bitrates, max_time, remux, iframes, file, scte35, ts_split, order_ascending, change_folder_chunks, testid });
 
 		driverReslut = ((BroadcasterCreateAddaptiveGroupDriver) testDriver).testIMPL(userName, userPass, login_ip, uiport, name, record,
-		zixi, hls, hds, mpd, mmt, compress_zixi, multicast, streams, bitrates, max_time, remux, iframes, file, scte35, ts_split, order_ascending );
+		zixi, hls, hds, mpd, mmt, compress_zixi, multicast, streams, bitrates, max_time, remux, iframes, file, scte35, ts_split, order_ascending, change_folder_chunks );
 		
 		memOnEnd = BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "ps v `pidof zixi_broadcaster` | tail -n 1 |  awk '{print $8}'");
 		Timestamp 	timestamp = new Timestamp(System.currentTimeMillis());
