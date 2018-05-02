404e7df3f1422f436db1a7bea2adaf5b348b8da5
urasigal@gmail.com <urasigal@gmail.com>
2018-05-02 11:22:06 -0300

    additional manual suite

--- src/test/java/com/zixi/testing/BroadcasterUpdateSettingsTest.java
+++ src/test/java/com/zixi/testing/BroadcasterUpdateSettingsTest.java
@@ -39,6 +39,40 @@ public class BroadcasterUpdateSettingsTest extends BasetTestZixiComponentRestart
 		// Checking if broadcaster has crashes while execution of the test.
 	}
 	
+	// Test parameters.
+		@Parameters({"userName","userPass","login_ip","uiport","server_id","gui_web_port","uname","aname","ft_max_quota","max_cpu","max_mem","max_in_bandwidth","max_out_bandwidth",
+		"admin_https","use_operator","use_user","use_observer","private_port","public_port", "testid"})
+		@Test
+		public void broadcasterUpdateSettingsSetDiffPorts(String userName,String userPass,String login_ip,String uiport,String server_id,String gui_web_port,String uname,
+		String aname,String ft_max_quota,String max_cpu,String max_mem,String max_in_bandwidth,String max_out_bandwidth,String admin_https,String use_operator,
+		String use_user,String use_observer,String private_port,String public_port, String range, String testid) throws Exception 
+		{
+			//Print this class name to the log file.
+			getLoggerInstance().info(getClass().getName());
+			
+			// Retrieve the product version. Parameters: 1 - host, 2 - user interface port, 3 - product login name, 4 - product login password.
+			productAboutDriver.getBroadcasterVersion(login_ip, uiport, userName, userPass);
+			
+			buildTestParametersString(new String[] { "userName","userPass","login_ip","uiport","server_id","gui_web_port","uname","aname","ft_max_quota","max_cpu","max_mem","max_in_bandwidth","max_out_bandwidth",
+			"admin_https","use_operator","use_user","use_observer","private_port","public_port", "range", "testid" }, 
+			new String[] {userName, userPass, login_ip, uiport, server_id, gui_web_port, uname, aname, ft_max_quota, max_cpu, max_mem, max_in_bandwidth, max_out_bandwidth, admin_https, use_operator,
+			use_user,use_observer,private_port,public_port, range, testid});
+			StringBuilder privatePorts = new StringBuilder();
+			int portRange = Integer.parseInt(range);
+			privatePorts.append(private_port).append(",,,0,0");
+			for (int i = 1; i < portRange; i++) {
+				int privateTmpPort = Integer.parseInt(private_port);
+				privateTmpPort ++;
+				privatePorts.append("&private_port=" + private_port).append(",,,0,0");
+			}
+			
+			driverReslut = ((BroadcasterUpdateSettingsDriver) testDriver).testIMPL(userName, userPass, login_ip, uiport, server_id, gui_web_port, uname, aname,
+			ft_max_quota, max_cpu, max_mem, max_in_bandwidth, max_out_bandwidth, admin_https, use_operator, use_user, use_observer, privatePorts.toString(), public_port); 
+			
+			Assert.assertEquals(driverReslut.getResult(), "GOOD");
+			// Checking if broadcaster has crashes while execution of the test.
+		}
+	
 		// Test parameters.
 		@Parameters({"userName","userPass","login_ip","uiport","server_id","gui_web_port","uname","aname","ft_max_quota","max_cpu","max_mem","max_in_bandwidth","max_out_bandwidth",
 		"admin_https","use_operator","use_user","use_observer","private_port","public_port", "testid"})
