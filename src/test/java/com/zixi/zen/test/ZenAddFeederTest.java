efeee87f31b655bc60f15ef857959aa3aad94427
urasigal@gmail.com <urasigal@gmail.com>
2018-05-28 12:51:45 -0300

    zen add feeder

--- src/test/java/com/zixi/zen/test/ZenAddFeederTest.java
+++ src/test/java/com/zixi/zen/test/ZenAddFeederTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import com.zixi.drivers.drivers.ZenAddFeederDriver;
+import com.zixi.drivers.tools.DriverReslut;
 import com.zixi.testing.BaseTestZixiMainComponents;
 
 // The goal of the test is to verify an ability of a feeder server to accept a private key file through an UI API request.
@@ -14,14 +15,13 @@ public class ZenAddFeederTest extends BaseTestZixiMainComponents{
 	public void testInit() { testDriver = new ZenAddFeederDriver(); }
 
 	// Test parameters - these parameters will be provided through an appropriate suite's XML file.
-	@Parameters({ "userName", "userPass", "login_ip", "uiport", "testid"})
+	@Parameters({ "userName", "userPass", "login_ip", "uiport", "feederName", "shhKeyName", "accessTagName", "testid"})
 	@Test
-	public void uploadSslCertificateToBroadcasterServer(String userName, String userPass, String login_ip, String uiport, String testid) throws Exception {
+	public void uploadSslCertificateToBroadcasterServer(String userName, String userPass, String login_ip, String uiport, String feederName, String shhKeyName, String accessTagName, String testid) throws Exception {
 		// Provide parameters to a TestLink.
-		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport", "testid"}, 
-		new String[] { userName, userPass, login_ip, uiport, testid });
-		
-		((ZenAddFeederDriver) testDriver).addFeeder(userName, userPass, login_ip, uiport);
-		//Assert.assertTrue(driverReslut.getResult().contains("SSL Certificate is uploaded successfully"));
+		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport", "feederName", "shhKeyName", "accessTagName", "testid"}, 
+		new String[] { userName, userPass, login_ip, uiport, feederName, shhKeyName, accessTagName, testid });
+		driverReslut = ((ZenAddFeederDriver) testDriver).addFeeder(userName, userPass, login_ip, uiport, feederName, shhKeyName, accessTagName);
+		Assert.assertEquals(driverReslut.getResult(), "true"); 
 	}
 }
