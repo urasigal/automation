db98f51f91a4f905e1d5cd9635f2e430ea8258a2
urasigal@gmail.com <urasigal@gmail.com>
2018-05-31 11:50:43 -0300

    complete feeder ZEN tunnel configuration.

--- src/test/java/com/zixi/testing/FeederSettingsTest.java
+++ src/test/java/com/zixi/testing/FeederSettingsTest.java
@@ -4,7 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.zixi.drivers.drivers.FeederReverseTunnelParametersDriver;
 import com.zixi.drivers.drivers.FeederSettingsDriver;
+import com.zixi.drivers.drivers.ZenFeedersData;
 
 public class FeederSettingsTest extends BaseTestZixiMainComponents{
 	
@@ -12,17 +15,20 @@ public class FeederSettingsTest extends BaseTestZixiMainComponents{
 	public void testInit() { testDriver = new FeederSettingsDriver(); }
 	
 	// Test parameters - these parameters will be provided through an appropriate suite's XML file.
-	@Parameters({ "userName", "userPass", "login_ip", "uiport", "host", "ssh_port", "ssh_user", "testid"})
+	@Parameters({ "userName", "userPass", "login_ip", "uiport", "host", "ssh_port", "ssh_user", "zenFeederName","testid"})
 	@Test
 	public void setFeederTunnelParameters(String userName, String userPass, String login_ip, String uiport, String host,
-	String ssh_port, String ssh_user, String testid) throws Exception {
+	String ssh_port, String ssh_user, String zenFeederName, String testid) throws Exception {
 		
 		// Provide parameters to a TestLink.
-		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport", "host", "ssh_port", "ssh_user", "testid"}, 
-		new String[] { userName, userPass, login_ip, uiport, host, ssh_port, ssh_user, testid });
+		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "uiport", "host", "ssh_port", "ssh_user", "zenFeederName","testid"}, 
+		new String[] { userName, userPass, login_ip, uiport, host, ssh_port, ssh_user, zenFeederName, testid });
 		
 		driverReslut = ( (FeederSettingsDriver) testDriver).testIMPL(userName, userPass, login_ip, uiport, host, ssh_port, ssh_user);
-		
+		FeederReverseTunnelParametersDriver feederReverseTunnelParametersDriver = new FeederReverseTunnelParametersDriver();
+		feederReverseTunnelParametersDriver.testIMPL(userName, userPass, login_ip, uiport, "1", "4200", "127.0.0.1" ,
+		ZenFeedersData.getZenFeederRemoteTunnelPort(userName, userPass, host, uiport, zenFeederName));
+
 		Assert.assertEquals(driverReslut.getResult(), host);
 	}
 
