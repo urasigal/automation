0ec55c3891afaa11dc0b4dcd6f13a4c9ff5854ef
urasigal@gmail.com <urasigal@gmail.com>
2018-06-04 13:10:51 -0300

    failover + zen cluster

---
+++ src/test/java/com/zixi/zen/test/ZenAddClusterTest.java
@@ -0,0 +1,37 @@
+package com.zixi.zen.test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.zixi.drivers.drivers.ZenAddFeederDriver;
+import com.zixi.testing.BaseTestZixiMainComponents;
+
+public class ZenAddClusterTest extends BaseTestZixiMainComponents{
+	
+	@BeforeClass
+	public void testInit() { testDriver = new ZenAddFeederDriver(); }
+
+	// Test parameters - these parameters will be provided through an appropriate suite's XML file.
+	@Parameters({ "zenUserName", "zenUserPass", "zenLogin_ip", "zenUiport", 
+	"clusterName", "resource_tag_name", "can_input", "can_output", "can_process", "http_streaming_port", "auth_mode",
+	"allow_unmanaged_inputs", "allow_unmanaged_outputs", "dns_prefix", "is_auto_scaling", "activation_key",
+	"bx_version", "aws_account_name", "api_user", "api_password", "testid"})
+	@Test
+	public void addFeederToZen(String zenUserName, String zenUserPass, String zenLogin_ip, String zenUiport, 
+	String clusterName, String resource_tag_name, String can_input, String can_output, String can_process, String http_streaming_port, String auth_mode,
+	String allow_unmanaged_inputs, String allow_unmanaged_outputs, String dns_prefix, String is_auto_scaling, String activation_key,
+	String bx_version, String aws_account_name, String api_user, String api_password, String testid) throws Exception {
+		// Provide parameters to a TestLink.
+		buildTestParametersString(new String[] {"zenUserName", "zenUserPass", "zenLogin_ip", "zenUiport", 
+		"clusterName", "resource_tag_name", "can_input", "can_output", "can_process", "http_streaming_port", "auth_mode",
+		"allow_unmanaged_inputs", "allow_unmanaged_outputs", "dns_prefix", "is_auto_scaling", "activation_key",
+		"bx_version", "aws_account_name", "api_user", "api_password", "testid"}, 
+		new String[] {zenUserName, zenUserPass, zenLogin_ip, zenUiport, 
+		clusterName, resource_tag_name, can_input, can_output, can_process, http_streaming_port, auth_mode,
+		allow_unmanaged_inputs, allow_unmanaged_outputs, dns_prefix, is_auto_scaling, activation_key,
+		bx_version, aws_account_name, api_user, api_password, testid});
+//		driverReslut = ((ZenAddFeederDriver) testDriver).addFeeder(userName, userPass, login_ip, uiport, feederName, feederUser, feederPass, shhKeyName, accessTagName);
+//		Assert.assertEquals(driverReslut.getResult(), "true"); 
+	}
+}
