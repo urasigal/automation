d77f8618b89e90def4b4b85af09e729c06cdd5fe
urasigal@gmail.com <urasigal@gmail.com>
2018-06-06 11:32:31 -0300

    ZEN add cluster fixes

--- src/test/java/com/zixi/zen/test/ZenAddClusterTest.java
+++ src/test/java/com/zixi/zen/test/ZenAddClusterTest.java
@@ -17,26 +17,26 @@ public class ZenAddClusterTest extends BaseTestZixiMainComponents{
 	@Parameters({ "zenUserName", "zenUserPass", "zenLogin_ip", "zenUiport", 
 	"clusterName", "resource_tag_name", "can_input", "can_output", "can_process", "http_streaming_port", "auth_mode",
 	"allow_unmanaged_inputs", "allow_unmanaged_outputs", "dns_prefix", "is_auto_scaling", "activation_key",
-	"bx_version", "aws_account_name", "region", "api_user", "api_password", "testid"})
+	"bx_version", "aws_account_name", "region", "min_size", "max_size", "api_user", "api_password", "testid"})
 	@Test
 	public void addFeederToZen(String zenUserName, String zenUserPass, String zenLogin_ip, String zenUiport, 
 	String clusterName, String resource_tag_name, String can_input, String can_output, String can_process, String http_streaming_port, String auth_mode,
 	String allow_unmanaged_inputs, String allow_unmanaged_outputs, String dns_prefix, String is_auto_scaling, String activation_key,
-	String bx_version, String aws_account_name, String region, String api_user, String api_password, String testid) throws Exception {
+	String bx_version, String aws_account_name, String region, String min_size, String max_size, String api_user, String api_password, String testid) throws Exception {
 		// Provide parameters to a TestLink.
 		buildTestParametersString(new String[] {"zenUserName", "zenUserPass", "zenLogin_ip", "zenUiport", 
 		"clusterName", "resource_tag_name", "can_input", "can_output", "can_process", "http_streaming_port", "auth_mode",
 		"allow_unmanaged_inputs", "allow_unmanaged_outputs", "dns_prefix", "is_auto_scaling", "activation_key",
-		"bx_version", "aws_account_name", "region", "api_user", "api_password", "testid"}, 
+		"bx_version", "aws_account_name", "region", "min_size", "max_size", "api_user", "api_password", "testid"}, 
 		new String[] {zenUserName, zenUserPass, zenLogin_ip, zenUiport, 
 		clusterName, resource_tag_name, can_input, can_output, can_process, http_streaming_port, auth_mode,
 		allow_unmanaged_inputs, allow_unmanaged_outputs, dns_prefix, is_auto_scaling, activation_key,
-		bx_version, aws_account_name, region, api_user, api_password, testid});
+		bx_version, aws_account_name, region, min_size, max_size, api_user, api_password, testid});
 		
 		driverReslut = ((ZenAddClusterDriver) testDriver).addCluster(zenUserName, zenUserPass, zenLogin_ip, zenUiport, 
 		clusterName, resource_tag_name, can_input, can_output, can_process, http_streaming_port, auth_mode,
 		allow_unmanaged_inputs, allow_unmanaged_outputs, dns_prefix, is_auto_scaling, activation_key,
-		bx_version, aws_account_name, region, api_user, api_password);
-//		Assert.assertEquals(driverReslut.getResult(), "true"); 
+		bx_version, aws_account_name, region, min_size, max_size, api_user, api_password);
+		Assert.assertEquals(driverReslut.getResult(), "true"); 
 	}
 }
