e5c0fe6d540dd679063a489c528c5333140cdda5
urasigal@gmail.com <urasigal@gmail.com>
2018-06-12 13:46:14 -0300

    Attach receiver to its ZEN entity.

---
+++ src/test/java/com/zixi/zen/test/ZenAttachReceiverToZenREceiverTest.java
@@ -0,0 +1,28 @@
+package com.zixi.zen.test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.zixi.drivers.drivers.ReceiverSshConnectionDriver;
+import com.zixi.testing.BaseTestZixiMainComponents;
+
+public class ZenAttachReceiverToZenREceiverTest  extends BaseTestZixiMainComponents{
+	
+	@BeforeClass
+	public void testInit() { testDriver = new ReceiverSshConnectionDriver(); }
+
+	// Test parameters - these parameters will be provided through an appropriate suite's XML file.
+	@Parameters({"receiverUserName", "receiverUserPass", "receiverLogin_ip", "receiverUiport", "sshHost", "ssh_port", "ssh_user", "testid"})
+	@Test
+	public void attachReceiverToREceiverZen(String receiverUserName,  String receiverUserPass, String receiverLogin_ip, 
+	String receiverUiport, String sshHost, String ssh_port, String ssh_user, String testid) throws Exception {
+		// Provide parameters to a TestLink.
+		buildTestParametersString(new String[] {"receiverUserName", "receiverUserPass", "receiverLogin_ip", "receiverUiport", "sshHost", "ssh_port", "ssh_user", "testid"}, 
+		new String[] {receiverUserName, receiverUserPass, receiverLogin_ip, receiverUiport, sshHost, ssh_port, ssh_user, testid});
+		driverReslut = ((ReceiverSshConnectionDriver) testDriver).setSshUserAndSshHostReceiver(
+						 receiverUserName,  receiverUserPass, receiverLogin_ip, receiverUiport, sshHost, ssh_port, ssh_user + "-receiver");
+		Assert.assertEquals(driverReslut.getResult(), "true"); 
+	}
+}
