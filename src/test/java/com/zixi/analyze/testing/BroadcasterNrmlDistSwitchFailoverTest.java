dea42f5fe0c32af56e487910b22fdd2ca7f08c47
urasigal@gmail.com <urasigal@gmail.com>
2018-06-29 16:39:00 -0300

    fix BroadcasterNrmlDistSwitchFailoverTest - set missing testInit method.

--- src/test/java/com/zixi/analyze/testing/BroadcasterNrmlDistSwitchFailoverTest.java
+++ src/test/java/com/zixi/analyze/testing/BroadcasterNrmlDistSwitchFailoverTest.java
@@ -4,18 +4,28 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.zixi.drivers.drivers.BroadcaserAllInputStreamDeletorDriver;
 import com.zixi.drivers.drivers.BroadcaserSingleOutputStreamDeletionDriver;
 import com.zixi.drivers.drivers.BroadcasterSwitchInputsFaioverNormalDistibutionDriver;
+import com.zixi.drivers.drivers.BroadcasterUdpOutputCreationDriver;
 import com.zixi.testing.BaseTestZixiMainComponents;
 
 public class BroadcasterNrmlDistSwitchFailoverTest extends BaseTestZixiMainComponents
 {	
+	
+	@BeforeClass
+	public void testInit() { testDriver = new BroadcasterSwitchInputsFaioverNormalDistibutionDriver(); }
+	
 	
 	@Parameters({ "remoteBX_login_ip", "remoteBXuserName", "remoteBXuserPassword", "remoteBXuiport",
 	"login_ip", "userName", "userPassword", "uiport", "testElapsedTime", "streamSwitchInterval", "streamReferenceName", "testid" })
 	@Test
 	public void broadcasterSwitchInputsFaioverNormalDistibutionTest(String remoteBX_login_ip, String remoteBXuserName, String remoteBXuserPassword, String remoteBXuiport,
     String login_ip, String userName, String userPassword, String uiport, String testElapsedTime, String streamSwitchInterval, String streamReferenceName, String testid) throws Exception {
+		
+		
+		
 		// Get broadcaster PID in the beginning of the test.
 		sutProcessId = BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "pidof zixi_broadcaster");
 		// Retrieve the product version. Parameters: 1 - host, 2 - user interface port, 3 - product login name, 4 - product login password.
