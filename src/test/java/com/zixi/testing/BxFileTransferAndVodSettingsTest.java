c3dc22bb69d72bd69acda5461f77c37906c4a264
urasigal@gmail.com <urasigal@gmail.com>
2018-08-01 16:01:04 -0300

    fix VOD zixi mode

--- src/test/java/com/zixi/testing/BxFileTransferAndVodSettingsTest.java
+++ src/test/java/com/zixi/testing/BxFileTransferAndVodSettingsTest.java
@@ -11,11 +11,11 @@ import com.zixi.drivers.drivers.BxFileTransferAndVodSettingsDriver;
 
 public class BxFileTransferAndVodSettingsTest extends BaseTestZixiMainComponents{
 	
-	@BeforeClass
+	@BeforeClass 
 	public void testInit() { testDriver = new BxFileTransferAndVodSettingsDriver(); }
 
 	@Parameters({ "userName","userPass" ,"login_ip", "uiport", "ft_download" , "ft_upload","ft_auto_index", "ft_prog", "ft_encrypt", "ft_bitrate_cache", "ft_aggr",
-	"ft_mtu", "ft_init_speed", "ft_cache", "ft_proxy_http_port", "ft_proxy_https_port", "max_download_bitrate", "max_upload_bitrate", "ft_zixi_index", "ft_zixi_index", "testid"})
+	"ft_mtu", "ft_init_speed", "ft_cache", "ft_proxy_http_port", "ft_proxy_https_port", "max_download_bitrate", "max_upload_bitrate","ft_zixi_index", "ft_zixi_index", "testid"})
 	@Test
 	public void broadcasterSetBxFileTransferVodSettings(String userName, String userPass, String login_ip, String uiport, String ft_download, String ft_upload,
 	String ft_auto_index, String ft_prog, String ft_encrypt, String ft_bitrate_cache, String ft_aggr, String ft_mtu, String ft_init_speed,
