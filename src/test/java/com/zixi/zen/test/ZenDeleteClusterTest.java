ec3768c86181cde90b33fd24524291eb708672c3
urasigal@gmail.com <urasigal@gmail.com>
2018-08-14 11:36:36 -0300

    Add delete user/role functionality to ZEN testing suites.

--- src/test/java/com/zixi/zen/test/ZenDeleteClusterTest.java
+++ src/test/java/com/zixi/zen/test/ZenDeleteClusterTest.java
@@ -24,4 +24,17 @@ public class ZenDeleteClusterTest extends BaseTestZixiMainComponentsZen {
 		driverReslut = ((ZenDeleteClusterDriver) testDriver).deleteCluster(zenUserName, zenUserPass, zenLogin_ip, zenUiport, clusterName);
 		Assert.assertEquals(driverReslut.getResult(), "true"); 
 	}
+	
+	// Test parameters - these parameters will be provided by appropriate suite's XML file.
+		@Parameters({ "zenUserName", "zenUserPass", "zenLogin_ip", "zenUiport", "clusterName", "testid"})
+		@Test
+		public void tryDeleteClusterFromZen(String zenUserName, String zenUserPass, String zenLogin_ip, String zenUiport, 
+		String clusterName, String testid) throws Exception {
+			// Provide parameters to a TestLink.
+			buildTestParametersString(new String[] {"zenUserName", "zenUserPass", "zenLogin_ip", "zenUiport", "clusterName", "testid"}, 
+			new String[] {zenUserName, zenUserPass, zenLogin_ip, zenUiport, clusterName, testid});
+			
+			driverReslut = ((ZenDeleteClusterDriver) testDriver).deleteCluster(zenUserName, zenUserPass, zenLogin_ip, zenUiport, clusterName);
+			Assert.assertEquals(driverReslut.getResult(), "{\"success\":false,\"error\":\"Unauthorized\"}"); 
+		}
 }
