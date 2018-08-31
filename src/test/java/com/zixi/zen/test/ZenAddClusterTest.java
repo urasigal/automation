d606b1d7f6406d571a85119642c68cbdef41af74
urasigal@gmail.com <urasigal@gmail.com>
2018-08-31 15:44:02 -0300

    fix

--- src/test/java/com/zixi/zen/test/ZenAddClusterTest.java
+++ src/test/java/com/zixi/zen/test/ZenAddClusterTest.java
@@ -11,12 +11,12 @@ public class ZenAddClusterTest extends BaseTestZixiMainComponentsZen{
 	
 	@BeforeClass
 	public void testInit() { testDriver = new ZenAddClusterDriver(); }
-
 	// Test parameters - these parameters will be provided by appropriate suite's XML file.
 	@Parameters({ "zenUserName", "zenUserPass", "zenLogin_ip", "zenUiport", 
 	"clusterName", "resource_tag_name", "can_input", "can_output", "can_process", "http_streaming_port", "auth_mode",
 	"allow_unmanaged_inputs", "allow_unmanaged_outputs", "dns_prefix", "is_auto_scaling", "activation_key",
-	"bx_version", "aws_account_name", "region", "key_pair", "security_group", "eips", "min_size", "max_size", "api_user", "api_password", "testid"})
+	"bx_version", "aws_account_name", "region", "key_pair", "security_group", "eips", "min_size", "max_size",
+	"api_user", "api_password", "vpc_name", "vpc_subnet_name", "testid"})
 	@Test
 	public void addClusterToZen(String zenUserName, String zenUserPass, String zenLogin_ip, String zenUiport, 
 	String clusterName, String resource_tag_name, String can_input, String can_output, String can_process, String http_streaming_port, String auth_mode,
