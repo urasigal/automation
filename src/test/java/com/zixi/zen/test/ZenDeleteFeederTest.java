4e5779775ea8a9ff5365c1ee936850a521ffe130
urasigal@gmail.com <urasigal@gmail.com>
2018-09-25 15:49:52 -0300

    Add delete feeder from ZEN

---
+++ src/test/java/com/zixi/zen/test/ZenDeleteFeederTest.java
@@ -0,0 +1,28 @@
+package com.zixi.zen.test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.zixi.drivers.drivers.ZenDeleteChannelDriver;
+import com.zixi.testing.BaseTestZixiMainComponentsZen;
+
+public class ZenDeleteFeederTest extends BaseTestZixiMainComponentsZen{
+	
+	@BeforeClass
+	public void testInit() { testDriver = new ZenDeleteChannelDriver(); }
+
+	// Delete channel.
+	@Parameters({"zenUser", "zenPass", "zenLogin_ip", "zenUiPort", "feeder_name", "testid"})
+	@Test
+	public void deleteSource(String zenUser, String zenPass, String zenLogin_ip, String zenUiPort, String feeder_name, String testid) throws Exception {
+		// Provide parameters to a TestLink.
+		buildTestParametersString(new String[] {"zenUser", "zenPass", "zenLogin_ip", "zenUiPort", "feeder_name", "testid"}, 
+		new String[] {zenUser, zenPass, zenLogin_ip, zenUiPort, feeder_name, testid});
+		
+		driverReslut = ((ZenDeleteChannelDriver) testDriver).deleteSource(zenUser, zenPass, zenLogin_ip, zenUiPort, feeder_name);
+		Assert.assertEquals(driverReslut.getResult(), "true"); 
+	}
+	
+}
