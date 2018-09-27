128ae8ae535b2b8f4bf1f4778642f7919949e9a8
urasigal@gmail.com <urasigal@gmail.com>
2018-09-27 11:17:01 -0300

    add test to udp target suite

--- src/test/java/com/zixi/zen/test/ZenAddUdpTargetTest.java
+++ src/test/java/com/zixi/zen/test/ZenAddUdpTargetTest.java
@@ -14,18 +14,18 @@ public class ZenAddUdpTargetTest extends BaseTestZixiMainComponentsZen{
 	public void testInit() { testDriver = new ZenAddTargetDriver(); }
 
 	@Parameters({"zenUser", "zenPass", "zenLogin_ip", "zenUiPort", "targetName", "resource_tag_ids", "targetType", "channel_name",
-	"host", "port", "bind_cidr", "rtp", "smpte_2022_fec", "smpte_2022_fec_cols", "smpte_2022_fec_rows", "smoothing", "testid"})
+	"host", "port", "bind_cidr", "rtp", "smpte_2022_fec", "smpte_2022_fec_cols", "smpte_2022_fec_rows", "smoothing", "remux_kbps", "testid"})
 	@Test
 	public void addUdpTarget(String zenUser, String zenPass, String zenLogin_ip, String zenUiPort, String targetName, String resource_tag_ids, String targetType, String channel_name,
-		String host, String port, String bind_cidr, String rtp, String smpte_2022_fec, String smpte_2022_fec_cols, String smpte_2022_fec_rows, String smoothing, String testid) throws Exception {
+		String host, String port, String bind_cidr, String rtp, String smpte_2022_fec, String smpte_2022_fec_cols, String smpte_2022_fec_rows, String smoothing, String remux_kbps, String testid) throws Exception {
 		// Provide parameters to a TestLink.
 		buildTestParametersString(new String[] {"zenUser", "zenPass", "zenLogin_ip", "zenUiPort", "targetName", "resource_tag_ids", "targetType", "channel_name",
-		"host", "port", "bind_cidr", "rtp", "smpte_2022_fec", "smpte_2022_fec_cols", "smpte_2022_fec_rows", "smoothing", "testid"}, 
+		"host", "port", "bind_cidr", "rtp", "smpte_2022_fec", "smpte_2022_fec_cols", "smpte_2022_fec_rows", "smoothing", "remux_kbps","testid"}, 
 		new String[] {zenUser, zenPass, zenLogin_ip, zenUiPort, targetName, resource_tag_ids, targetType, channel_name,
-		host, port, bind_cidr, rtp, smpte_2022_fec, smpte_2022_fec_cols, smpte_2022_fec_rows, smoothing, testid});
+		host, port, bind_cidr, rtp, smpte_2022_fec, smpte_2022_fec_cols, smpte_2022_fec_rows, smoothing, remux_kbps, testid});
 		
 		driverReslut = ((ZenAddTargetDriver) testDriver).addUdpRtpTarget( zenUser, zenPass, zenLogin_ip, zenUiPort, targetName, resource_tag_ids, targetType, channel_name,
-				host, port, bind_cidr, rtp, smpte_2022_fec, smpte_2022_fec_cols, smpte_2022_fec_rows, smoothing);
+				host, port, bind_cidr, rtp, smpte_2022_fec, smpte_2022_fec_cols, smpte_2022_fec_rows, smoothing, remux_kbps);
 		Assert.assertEquals(driverReslut.getResult(), "true"); 
 	}
 	
