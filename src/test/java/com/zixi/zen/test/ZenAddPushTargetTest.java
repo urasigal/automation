74197d2403f10d290fae46b31c4d515b24a37976
urasigal@gmail.com <urasigal@gmail.com>
2018-09-28 12:29:21 -0300

    fix ZenAddPushTargetTest

--- src/test/java/com/zixi/zen/test/ZenAddPushTargetTest.java
+++ src/test/java/com/zixi/zen/test/ZenAddPushTargetTest.java
@@ -19,7 +19,7 @@ public class ZenAddPushTargetTest extends BaseTestZixiMainComponentsZen{
 	public void addPushTarget(String zenUser, String zenPass, String zenLogin_ip, String zenUiPort, String name, String resource_tag_ids, String type, String channel_name,
 		String is_bonding, String target, String alt_target, String stream_id, String password, String latency, String zixi_encryption_key, String testid) throws Exception {
 		// Provide parameters to a TestLink.
-		buildTestParametersString(new String[] {"zenUser", "zenPass", "zenLogin_ip", "zenUiPort", "name", "resource_tag_ids", "type", "adaptive_channel_id", "delivery_channel_id",
+		buildTestParametersString(new String[] {"zenUser", "zenPass", "zenLogin_ip", "zenUiPort", "name", "resource_tag_ids", "type", "channel_name",
 		"is_bonding", "target", "alt_target", "stream_id", "password", "latency", "zixi_encryption_key", "testid"}, 
 		new String[] {zenUser, zenPass, zenLogin_ip, zenUiPort, name, resource_tag_ids, type, channel_name,
 		is_bonding, target, alt_target, stream_id, password, latency, zixi_encryption_key, testid});
