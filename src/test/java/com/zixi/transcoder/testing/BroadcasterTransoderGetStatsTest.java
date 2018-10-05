3c378248c49bfc16bc8385c591082cd1e43dfc5a
urasigal@gmail.com <urasigal@gmail.com>
2018-10-05 10:08:53 -0300

    add h24 to 265 intel hw 1080p60

--- src/test/java/com/zixi/transcoder/testing/BroadcasterTransoderGetStatsTest.java
+++ src/test/java/com/zixi/transcoder/testing/BroadcasterTransoderGetStatsTest.java
@@ -22,7 +22,15 @@ public class BroadcasterTransoderGetStatsTest extends BaseTestZixiMainComponents
 		new String[] {userName, userPass, login_ip, uiport, stream_name, testid});
 		
 		driverReslut = ((BroadcasterTransoderGetStatsDriver) testDriver).getTranscoderStatistics(userName, userPass, login_ip, uiport, stream_name, testid);
-		
+		//1 muxer_resets,
+		//2 decoder_resets, 
+		//3 encoder_resets,
+		//4 muxer_drops,
+		//5 muxer_resets,
+		//6 raw_frame_drops,
+		//7 restarts,
+		//8 smoother_drops,
+		//9 src_cmp_frame_drops
 		Assert.assertEquals( driverReslut.getResult(), "[0,0,0,0,0,0,0,0,0]");
 		// Checking if broadcaster has crashes while execution of the test.
 		Assert.assertEquals(sutProcessId, BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "pidof zixi_broadcaster"));
