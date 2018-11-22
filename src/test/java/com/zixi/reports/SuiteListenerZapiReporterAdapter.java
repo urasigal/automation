35c796b56872e3ff7c0fdc97293c6e5f6ad819c3
urasigal <urasigal@gmail.com>
2018-11-22 16:43:24 +0200

    fixes

--- src/test/java/com/zixi/reports/SuiteListenerZapiReporterAdapter.java
+++ src/test/java/com/zixi/reports/SuiteListenerZapiReporterAdapter.java
@@ -9,13 +9,16 @@ import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 
 import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import com.zixi.drivers.drivers.FeederPostKeyDriver;
 import com.zixi.zapi.ZapiCycleIntegrator;
 import com.zixi.zapi.ZapiExecutionProps;
 
-public class SuiteListenerZapiReporterAdapter extends SuiteListenerZapiReporter {
+public class SuiteListenerZapiReporterAdapter implements ISuiteListener, ITestListener{
 	private boolean execStatus = true;
 	private StringBuffer testFlowDescription = new StringBuffer();
 	private int testStepCnt = 1;
@@ -47,7 +50,7 @@ public class SuiteListenerZapiReporterAdapter extends SuiteListenerZapiReporter 
 	
 	@Override
 	public void onFinish(ISuite suite) {
-		String status       	= null;
+		String status       	= "";
 		String projectId		= suite.getParameter("projectId");
 		String issueId			= suite.getParameter("issueId");
 		String cycleId			= suite.getParameter("cycleId");
@@ -95,5 +98,29 @@ public class SuiteListenerZapiReporterAdapter extends SuiteListenerZapiReporter 
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
 	}
 }
