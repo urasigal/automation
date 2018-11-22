0c5bc8accedc3eef239ffce4a8216e37ea52b2a8
urasigal <urasigal@gmail.com>
2018-11-22 11:19:38 -0400

    Update SuiteListenerZapiReporterAdapter.java

--- src/test/java/com/zixi/reports/SuiteListenerZapiReporterAdapter.java
+++ src/test/java/com/zixi/reports/SuiteListenerZapiReporterAdapter.java
@@ -18,12 +18,29 @@ import com.zixi.drivers.drivers.FeederPostKeyDriver;
 import com.zixi.zapi.ZapiCycleIntegrator;
 import com.zixi.zapi.ZapiExecutionProps;
 
-public class SuiteListenerZapiReporterAdapter implements ISuiteListener{
+public class SuiteListenerZapiReporterAdapter implements ISuiteListener, ITestListener{
 	private boolean execStatus = true;
 	private StringBuffer testFlowDescription = new StringBuffer();
 	private int testStepCnt = 1;
 	
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		testStepCnt++;
+	}
 	
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		testFlowDescription.append("Test suite: " + result.getTestContext().getSuite().getName() + ", Step seq num: " + testStepCnt + " " +
+		result.getTestContext().getName() + " Status: Failed. " + result.getThrowable().getMessage().replaceAll("[^a-zA-Z0-9]", ""));
+		execStatus = false;
+	}
 	
 	@Override
 	public void onStart(ISuite suite) {
@@ -33,55 +50,30 @@ public class SuiteListenerZapiReporterAdapter implements ISuiteListener{
 	
 	@Override
 	public void onFinish(ISuite suite) {
-		String status       	= "";
-		String projectId		= suite.getParameter("projectId");
-		String issueId			= suite.getParameter("issueId");
-		String cycleId			= suite.getParameter("cycleId");
-		String versionId		= suite.getParameter("versionId");
-		String assigneeType		= suite.getParameter("assigneeType");
-		String zapiAccesskey	= suite.getParameter("zapiAccesskey");
-		String zapiSecretkey	= suite.getParameter("zapiSecretkey");
-		String folderId			= suite.getParameter("folderId");
-		String zapiUser			= suite.getParameter("zapiUser");
-	
-
-		if(cycleId.equals("")) {
-			String line;
-			try (InputStream fis = new FileInputStream("src/main/resources/cycleid");
-				InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-				BufferedReader br = new BufferedReader(isr);) {
-			while ((line = br.readLine()) != null) {
-				cycleId = line;
-				}
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				System.out.println("SuiteListenerZapiReporterAdapter.onFinish()" + e1.getMessage());
-			}
-		}
 		
-		try {
-			zapiAccesskey = FeederPostKeyDriver.getStringFromUrl("zapiAccesskey");
-			zapiAccesskey = FeederPostKeyDriver.getStringFromUrl("zapiSecretkey");
-			folderId = ZapiCycleIntegrator.getFolderIdFromCycle( cycleId, versionId,  projectId,  folderId,  zapiUser, 
-					 zapiAccesskey,  zapiSecretkey);
-		} catch (URISyntaxException | IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			if(execStatus == true)
-				status = "1"; // Passed.
-			else status = "2"; // Passed.
-			ZapiExecutionProps.createNewTestExecutionWithStatus_TestCycle_TestFolder(status, projectId, issueId, cycleId, folderId, 
-			versionId, assigneeType, zapiUser, zapiAccesskey, zapiSecretkey, testFlowDescription.toString());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
-	
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
 }
