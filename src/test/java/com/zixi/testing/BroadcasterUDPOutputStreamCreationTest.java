1dcb0b7a18bd87522704632bb004439d7f6157d2
urasigal@gmail.com <urasigal@gmail.com>
2019-05-16 13:24:14 -0300

    Updates -  update udp output setup - mtu.

--- src/test/java/com/zixi/testing/BroadcasterUDPOutputStreamCreationTest.java
+++ src/test/java/com/zixi/testing/BroadcasterUDPOutputStreamCreationTest.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import com.zixi.drivers.drivers.BroadcaserSingleOutputStreamDeletionDriver;
@@ -15,24 +16,27 @@ public class BroadcasterUDPOutputStreamCreationTest extends BaseTestZixiMainComp
 	public void testInit() { testDriver = new BroadcasterUdpOutputCreationDriver(testFlowDescriptor); }
 
 	@Parameters({ "userName", "userPass", "login_ip", "port", "stream", "streamname", "host", "id", "rtp", "fec", "smoothing", "ttl",
-	"remux_bitrate", "df", "local_port", "dec_key", "type", "rows", "remux_buff", "local_ip", "remux_restampdts", "uiport", "remux_pcr", "dec_type", "cols" ,"testid"})
+	"remux_bitrate", "df", "local_port", "dec_key", "type", "rows", "remux_buff", "local_ip", "remux_restampdts", "uiport", "remux_pcr",
+	"dec_type", "cols", "max_payload", "mtu_time", "testid"})
 	@Test
 	public void broadcasterPullInCreation(String userName, String userPass, String login_ip, String port, String stream, String streamname,
 	String host, String id, String rtp, String fec, String smoothing, String ttl, String remux_bitrate, String df, String local_port,
 	String dec_key, String type, String rows, String remux_buff, String local_ip, String remux_restampdts, String uiport, String remux_pcr,
-	String dec_type, String cols,String testid) throws Exception {
+	String dec_type, String cols, @Optional("0") String max_payload, @Optional("0") String mtu_time, String testid) throws Exception {
+		
 		testFlowDescriptor.append("\nStarting the BroadcasterUDPOutputStreamCreationTest test");
 		sutProcessId = BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "pidof zixi_broadcaster");		
 		productAboutDriver.getBroadcasterVersion(login_ip, uiport, userName, userPass);
 		memOnStart = BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "ps v `pidof zixi_broadcaster` | tail -n 1 |  awk '{print $8}'");
+		
 		buildTestParametersString(new String[] { "userName", "userPass", "login_ip", "port", "stream",
 		"streamname", "host", "id", "rtp", "fec", "smoothing", "ttl", "remux_bitrate", "df", "local_port", "dec_key", "type", "rows",
-		"remux_buff", "local_ip", "remux_restampdts", "uiport", "remux_pcr", "dec_type", "cols" ,"testid"}, 
-		new String[] {userName, userPass, login_ip, port, stream, streamname, host, id, rtp, fec, smoothing, ttl, remux_bitrate, df, local_port, dec_key, type, rows,
-		remux_buff, local_ip, remux_restampdts, uiport, remux_pcr, dec_type, cols ,testid });
+		"remux_buff", "local_ip", "remux_restampdts", "uiport", "remux_pcr", "dec_type", "cols", "max_payload", "mtu_time", "testid"}, 
+		new String[] {userName, userPass, login_ip, port, stream, streamname, host, id, rtp, fec, smoothing, ttl, remux_bitrate, df, local_port, 
+		dec_key, type, rows, remux_buff, local_ip, remux_restampdts, uiport, remux_pcr, dec_type, cols, max_payload, mtu_time, testid });
 		
 		driverReslut = ((BroadcasterUdpOutputCreationDriver) testDriver).testIMPL(userName, userPass, login_ip, port, stream, streamname, host, id, rtp, fec, smoothing,
-		ttl, remux_bitrate, df, local_port, dec_key, type, rows, remux_buff, local_ip, remux_restampdts, uiport, remux_pcr, dec_type, cols);
+		ttl, remux_bitrate, df, local_port, dec_key, type, rows, remux_buff, local_ip, remux_restampdts, uiport, remux_pcr, dec_type, cols, max_payload, mtu_time);
 		
 		memOnEnd = BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "ps v `pidof zixi_broadcaster` | tail -n 1 |  awk '{print $8}'");
 		Timestamp 	timestamp = new Timestamp(System.currentTimeMillis());
