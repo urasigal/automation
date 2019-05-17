1abf4f4267af70b35aac7bec307cc51c67d38e9e
urasigal@gmail.com <urasigal@gmail.com>
2019-05-17 16:51:11 -0300

    Add more failover tests

--- src/test/java/com/zixi/analyze/testing/BroadcasterNrmlDistSwitchFailoverTest.java
+++ src/test/java/com/zixi/analyze/testing/BroadcasterNrmlDistSwitchFailoverTest.java
@@ -27,13 +27,14 @@ public class BroadcasterNrmlDistSwitchFailoverTest extends BaseTestZixiMainCompo
 		sutProcessId = BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "pidof zixi_broadcaster");
 		// Retrieve the product version.
 		this.version = productAboutDriver.getBroadcasterVersion(login_ip, uiport, userName, userPassword);	
+		
 		buildTestParametersString(new String[] {"remoteBX_login_ip", "remoteBXuserName", "remoteBXuserPassword", "remoteBXuiport",
 		"login_ip", "userName", "userPassword", "uiport", "testElapsedTime", "streamSwitchInterval", "streamReferenceName", "testid"}, 
 		new String[] {remoteBX_login_ip, remoteBXuserName, remoteBXuserPassword, remoteBXuiport,
 		login_ip, userName, userPassword, uiport, testElapsedTime, streamSwitchInterval,streamReferenceName,  testid});
 		
-		driverReslut = ((BroadcasterSwitchInputsFaioverNormalDistributionDriver) testDriver).testNormalDistributioInputSwitching(remoteBX_login_ip, remoteBXuserName, remoteBXuserPassword,
-		remoteBXuiport, login_ip, userName, userPassword, uiport, testElapsedTime, streamSwitchInterval, streamReferenceName);	
+		driverReslut = ((BroadcasterSwitchInputsFaioverNormalDistributionDriver) testDriver).testNormalDistributioInputSwitching(remoteBX_login_ip, remoteBXuserName, 
+		remoteBXuserPassword, remoteBXuiport, login_ip, userName, userPassword, uiport, testElapsedTime, streamSwitchInterval, streamReferenceName);	
 		Assert.assertEquals(driverReslut.getResult(), "passed");
 		
 		Assert.assertEquals(sutProcessId, BroadcaserSingleOutputStreamDeletionDriver.getPid("root",  "zixiroot1234",  login_ip,  "22",  "pidof zixi_broadcaster"));
